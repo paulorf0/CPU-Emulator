2f6e06dee713837d94d65a93a8061a8f1e48dfa6
paulorf0 <pauloricardf180@gmail.com>
2025-05-23 17:50:51 -0300

    fix

--- software/cycle.h
+++ software/cycle.h
@@ -14,7 +14,7 @@ static inline void fetch(CPU *cpu){
     uint8_t size = get_instruction_size((OPCODE)opcode);
 
     for(uint8_t i = 0; i < size; i ++)
-        cpu->reg.MBR = ((cpu->reg.MBR<<8 ) | cpu->memory.memory[cpu->reg.MAR++]);
+        cpu->reg.MBR = ((cpu->reg.MBR<<8 ) | cpu->memory.memory[cpu->reg.MAR + i]);
 
     // The last eight bits are not used.
     cpu->reg.MBR = cpu->reg.MBR & 0xFFFFFF00;
@@ -26,49 +26,49 @@ static inline void decode(CPU *cpu){
     // OpCode instruction
     cpu->reg.IR = OPCODE5(cpu->reg.MBR);
     switch(cpu->reg.IR){
-    L_hlt:
-    L_nop:
+    case HLT:
+    case NOP:
         break;
-    L_add:
-    L_ldr:
-    L_str:
-    L_sub:
-    L_mul:
-    L_div:
-    L_cmp:
-    L_movr:
-    L_and:
-    L_or:
-    L_xor:
+    case ADD:
+    case LDR:
+    case STR:
+    case SUB:
+    case MUL:
+    case DIV:
+    case CMP:
+    case MOVR:
+    case AND:
+    case OR:
+    case XOR:
         // 0 - 4 (OPCODE), 4 - 6 (Reg0), 6 - 8 (Reg1) (The bits were numbered left to right)
         // The buffer has 32 bits, but just 24 bits is used.
         cpu->reg.RO0 = (cpu->reg.MBR >> 25) & 0x03;
         cpu->reg.RO1 = (cpu->reg.MBR >> 23) & 0x03;
         break;
-    L_not:
+    case NOT:
         cpu->reg.RO0 = (cpu->reg.MBR >> 26 ) & 0x03;
         break;
-    L_je:
-    L_jne:
-    L_jl:
-    L_jle:
-    L_jg:
-    L_jge:
-    L_jmp:
-        cpu->reg.PC = (cpu->reg.MBR >> 9) & 0xFFFF; // Maybe this is wrong
+    case JE:
+    case JNE:
+    case JL:
+    case JLE:
+    case JG:
+    case JGE:
+    case JMP:
+        cpu->reg.MAR = (cpu->reg.MBR >> 9) & 0xFFFF; // Maybe this is wrong
         break;
-    L_ld:
-    L_st:
+    case LD:
+    case ST:
         cpu->reg.MAR = (cpu->reg.MBR >> 9) & 0xFFFF;
         cpu->reg.RO0 = (cpu->reg.MBR >> 26) & 0x03;
         break;
-    L_movi:
-    L_addi:
-    L_subi:
-    L_muli:
-    L_divi:
-    L_lsh:
-    L_rsh:
+    case MOVI:
+    case ADDI:
+    case SUBI:
+    case MULI:
+    case DIVI:
+    case LSH:
+    case RSH:
         cpu->reg.IMM = (cpu->reg.MBR >> 9) & 0xFFFF;
         cpu->reg.RO0 = (cpu->reg.MBR >> 26) & 0x03;
         break;
